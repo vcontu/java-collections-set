4942aa3a3cbe86dff4c7918a6342e9a9fd37c423
timofeifurdui <Timofei.Furdui@endava.com>
2022-03-24 20:57:48 +0200

    Added implementations for some methods in HashSet task

--- src/main/java/com/endava/internship/collections/Main.java
+++ src/main/java/com/endava/internship/collections/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Set<Student> studentSet = new StudentSet();
 
-        Student s1 = new Student("Jora", LocalDate.of(1998, 7, 23), "Boschetar");
-        Student s2 = new Student("Clava", LocalDate.of(1988, 11, 19), "Mulgatoare");
-        Student s3 = new Student("Aurel", LocalDate.of(1971, 3, 30), "Tractorist");
-        Student s4 = new Student("Varvara", LocalDate.of(1991, 9, 14), "Deritecatoare");
-        Student s5 = new Student("Fanel", LocalDate.of(2000, 2, 13), "Alcoolic");
+        Student s1 = new Student("Andrei", LocalDate.of(1998, 7, 23), "Student anul I");
+        Student s2 = new Student("Tatiana", LocalDate.of(1988, 11, 19), "Studenta anul I");
+        Student s3 = new Student("Victor", LocalDate.of(1971, 3, 30), "Student anul III");
+        Student s4 = new Student("Olga", LocalDate.of(1991, 9, 14), "Studenta anul IV");
+        Student s5 = new Student("Mihai", LocalDate.of(2000, 2, 13), "Student anul II");
         studentSet.add(s1);
         studentSet.add(s2);
         studentSet.add(s3);
@@ -24,7 +24,7 @@ public class Main {
         System.out.println(studentSet.contains(s5));
         System.out.println(studentSet.iterator());
         System.out.println(Arrays.toString(studentSet.toArray()));
-        System.out.println(studentSet.add(new Student("Zohan", LocalDate.of(1995, 12,25), "Frizer")));
+        System.out.println(studentSet.add(new Student("Tudor", LocalDate.of(1995, 12,25), "Student anul II")));
         System.out.println(studentSet.remove(s5));
 //        System.out.println(studentSet.clear());
     }
